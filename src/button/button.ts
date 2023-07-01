b045d72b2daadd03553cbbe7646345602e9607ec
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-07-01 17:30:49 +0200

    Update button.ts

--- src/button/button.ts
+++ src/button/button.ts
@@ -5,65 +5,64 @@ import { LitElement, html } from 'lit'
 @customElement()
 export class CustomButton extends LitElement {
   label: string
+  hasIcon: boolean
+  hasLabel: boolean
   type: 'elevated' | 'filled' | 'text' | 'outlined' = 'text'
 
   static properties = {
-    label: { type: String }
+    label: { type: String },
+    hasIcon: { type: Boolean, reflect: true, attribute: 'has-icon' },
+    hasLabel: { type: Boolean, reflect: true, attribute: 'has-label' }
   }
 
-  constructor() {
-    super()
-  }
-
-  async connectedCallback(): Promise<void> {
-    super.connectedCallback()
+  async connectedCallback() {
+    const slots = Array.from(this.querySelectorAll('slot'))
+    for (const slot of slots) {
+      slot.addEventListener('slotchange', this.#slotchange)
+    }
+    super.connectedCallback();
     await this.updateComplete
-    this.requestUpdate()
+    this.#updateAttributes()
   }
 
-  renderLabel() {
+  #updateAttributes() {
     const iconSlot = this.shadowRoot.querySelector('slot[name="icon"]')
     // @ts-ignore
-    const children = Array.from(iconSlot?.assignedNodes() || [])
-    return children.length !== 0 ? 
-        html`<span class="label with-icon"> ${this.label} </span>` :
-        html`<span class="label"> ${this.label} </span>`;
+    this.hasIcon = (Array.from(iconSlot?.assignedNodes() || [])).length !== 0
+    this.hasLabel = Boolean(this.label?.length > 0)
   }
 
+  #slotchange = () => this.#updateAttributes()
 
   render() {
     return html`
     <style>
       :host {
-        display: contents:
+        display: contents;
+        color: var(--md-sys-color-on-background);
       }
 
       button {
+        display: inline-flex;
         height: 40px;
         box-sizing: border-box;
-        border: none;
         cursor: pointer;
         border-radius: 20px;
         position: relative;
         background: transparent;
-
-        color: var(--md-sys-color-on-background);
+        color: inherit;
+        align-items: center;
+        justify-content: center;
+        border: none;
+        user-select: none;
+        outline: none;
       }
 
       .label {
-        padding: 16px;
         font-family: var(--md-sys-typescale-label-large-font-family-name);
         font-size: var(--md-sys-typescale-label-large-font-size);
         line-height: var(--md-sys-typescale-label-large-line-height);
         letter-spacing: var(--md-sys-typescale-label-large-letter-spacing);
-      }
-
-      .label.with-icon {
-        padding: 0 16px 0 8px;
-      }
-
-      slot[name="icon"]::slotted(*) {
-        padding-left: 16px;
       }
 
       :host([type="filled"]) button {
@@ -92,7 +91,6 @@ export class CustomButton extends LitElement {
       }
 
       custom-elevation {
-
         --md-elevation-level: var(--elevation-level);
       }
 
@@ -103,11 +101,29 @@ export class CustomButton extends LitElement {
       :host([type="filled"]) custom-elevation, :host([type="outlined"]) custom-elevation, :host([type="outlined"]) custom-elevation {
         --md-elevation-level: 0;
       }
+
+      :host([has-label]) .label {
+        padding-left: 24px;
+        padding-right: 24px;
+      }
+
+      :host([has-icon]:not([has-label])) button {
+        border-radius: 50%;
+        width: 40px;
+      }
+
+      :host([has-icon][has-label]) .label {
+        padding-left: 8px;
+        padding-right: 24px;
+      }
+      :host([has-icon][has-label]) slot[name="icon"]::slotted(*) {
+        padding-left: 16px;
+      }
     </style>
     <button label=${this.label}>
-    <custom-elevation></custom-elevation>
+      <custom-elevation></custom-elevation>
       <slot name="icon"></slot>
-      ${this.renderLabel()}
+      <span class="label"> ${this.label} </span>
     </button>
     `
   }
