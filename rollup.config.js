8d3329faf3def9ba0f14a8fd96e1295cb8d7055a
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-07-02 12:58:40 +0200

    Automate exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,36 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import { cp } from "fs/promises";
+import { cp, readFile, writeFile } from "fs/promises";
 import { globby } from 'globby'
+import { parse } from "path";
 import { rimraf } from "rimraf";
 
 const input = await globby('src/**/*.ts')
 
 const onlyUsedVariables = () => ({
   name: 'rollup-plugin-only-used-variables',
-  transform: (code, map, ast) => {
-    // console.log(code);
+  writeBundle: async (options, bundle) => {
+    const packageExports = {
+      '.': {
+        import: './exports/custom-elements.js',
+        types: './exports/custom-elements.d.ts'
+      }
+    }
+    const glob = await globby('exports/**/*.d.ts')
+    
+    for (const path of glob) {
+      const parsed = parse(path)
+      const name = `./${parsed.name.replace('.d', '')}.js`
+      packageExports[name] = {
+        import: `./${path.replace('.d.ts', '.js')}`,
+        types: `./${path}`
+      }
+    }
+
+    const packageJSON = JSON.parse((await readFile('./package.json')).toString())
+
+    packageJSON.exports = packageExports
+    await writeFile('./package.json', JSON.stringify(packageJSON, null, '\t'))
   }
 })
 
@@ -17,10 +38,12 @@ const cleanBuild = () => ({
   name: 'clean',
   buildStart: async () => {
     rimraf('./exports/**/*.js', {glob: true})
+    rimraf('./exports/**/*.d.ts', {glob: true})
   }
 })
 
 await cp('src/theme/themes/default', 'exports/themes/default', {recursive: true})
+
 export default [{
   input,
   output: [{
