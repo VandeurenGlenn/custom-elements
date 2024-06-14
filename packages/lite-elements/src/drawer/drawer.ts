86f6030e23eb45680f071e1616039ccd780bb7ca
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-06-14 15:35:08 +0200

    Upgrade custom-shared-styles

--- packages/lite-elements/src/drawer/drawer.ts
+++ packages/lite-elements/src/drawer/drawer.ts
@@ -2,6 +2,7 @@ import { customElement, LiteElement, html, property } from '@vandeurenglenn/lite
 import '../elevation/elevation.js'
 import '../button/button.js'
 import '../pane/pane.js'
+import style from '@vandeurenglenn/custom-shared-styles/drawer.css'
 
 @customElement('custom-drawer')
 export class CustomDrawer extends LiteElement {
@@ -30,64 +31,16 @@ export class CustomDrawer extends LiteElement {
     })
   }
 
+  static styles? = [style]
+
   render() {
-    return html`<style>
-        :host {
-          display: contents;
-        }
-
-        custom-pane {
-          box-sizing: border-box;
-          padding: 12px 24px;
-          height: 100%;
-          --custom-pane-width: var(--custom-drawer-with, 320px);
-        }
-
-        slot[name='headline']::slotted(*) {
-          color: var(--md-sys-color-on-surface-variant);
-          font-family: var(--md-sys-typescale-title-small-font-family-name);
-          font-style: var(--md-sys-typescale-title-small-font-family-style);
-          font-weight: var(--md-sys-typescale-title-small-font-weight);
-          font-size: var(--md-sys-typescale-title-small-font-size);
-          letter-spacing: var(--md-sys-typescale-title-small-tracking);
-          line-height: var(--md-sys-typescale-title-small-height);
-          text-transform: var(--md-sys-typescale-title-small-text-transform);
-          text-decoration: var(--md-sys-typescale-title-small-text-decoration);
-          text-transform: capitalize;
-          margin: 0;
-        }
-
-        ::slotted([slot='footer']) {
-          display: block;
-          box-sizing: border-box;
-          min-height: 48px;
-          border-top: 1px solid rgba(0, 0, 0, 0.14);
-        }
-
-        ::slotted([slot='content']) {
-          display: flex;
-          flex-direction: column;
-          height: 100%;
-          width: 100%;
-          overflow-y: auto;
-        }
-
-        ::slotted(*) {
-          pointer-events: none;
-        }
-
-        aside {
-          display: flex;
-          flex-direction: column;
-          width: 100%;
-        }
-      </style>
-
+    return html`
       <custom-pane .open=${this.open} .mobile=${this.mobile} .type=${this.type} .id=${this.id}>
         <slot name="headline" slot="headline"></slot>
         <slot name="menu-button" slot="menu-button"></slot>
         <slot name="content" slot="content"></slot>
         <slot name="footer" slot="footer"></slot>
-      </custom-pane> `
+      </custom-pane>
+    `
   }
 }
--- packages/lite-elements/src/pane/pane.ts
+++ packages/lite-elements/src/pane/pane.ts
@@ -2,6 +2,7 @@ import { customElement, LiteElement, css, html, property, query, assignedElement
 import '../elevation/elevation.js'
 import '../button/icon-button.js'
 import '../icon/icon.js'
+import style from '@vandeurenglenn/custom-shared-styles/pane.css'
 
 @customElement('custom-pane')
 export class CustomPane extends LiteElement {
@@ -31,87 +32,10 @@ export class CustomPane extends LiteElement {
     )
   }
 
+  static styles = [style]
+
   render() {
-    return html`<style>
-        :host {
-          --custom-pane-width: 100%;
-          display: flex;
-          flex-direction: column;
-          height: 100%;
-          background: var(--md-sys-color-surface);
-          color: var(--md-sys-color-on-surface);
-          position: relative;
-          /* border-radius: 12px; */
-
-          --md-elevation-level: 0;
-
-          pointer-events: none;
-          opacity: 0;
-          width: 100%;
-          max-width: var(--custom-pane-width);
-          transition: var(--md-sys-motion-easing-emphasized-accelerate) 200ms opacity,
-            var(--md-sys-motion-easing-emphasized-accelerate) 200ms transform;
-
-          --custom-pane-footer-height: 54px;
-        }
-
-        :host([left]) {
-          border-radius: var(--md-sys-shape-corner-large-end);
-          transform: translateX(-100%);
-          z-index: 1002;
-        }
-
-        :host([right]) {
-          border-radius: var(--md-sys-shape-corner-large-start);
-          transform: translateX(100%);
-          z-index: 1001;
-        }
-
-        :host([mobile]) {
-          inset: 0;
-          position: fixed;
-          z-index: 1001;
-        }
-
-        :host([type='modal']) {
-          --md-elevation-level: 1;
-        }
-
-        :host([open]) {
-          transform: translateX(0);
-          opacity: 1;
-          pointer-events: auto;
-          transition: var(--md-sys-motion-easing-emphasized-decelerate) 500ms opacity,
-            var(--md-sys-motion-easing-emphasized-decelerate) 500ms transform;
-        }
-
-        :host([open]) {
-          position: relative;
-        }
-
-        :host([open][mobile]) {
-          position: fixed;
-        }
-
-        aside {
-          width: 100%;
-          height: 100%;
-        }
-
-        .content {
-          height: calc(100% - var(--custom-pane-footer-height));
-          width: 100%;
-          overflow-y: auto;
-        }
-
-        .footer {
-          height: var(--custom-pane-footer-height);
-        }
-
-        :host([right]) custom-icon-button {
-          transform: rotateZ(180deg);
-        }
-      </style>
+    return html`
       <custom-elevation></custom-elevation>
       <aside>
         <slot name="header">
@@ -128,6 +52,7 @@ export class CustomPane extends LiteElement {
         <flex-row class="footer">
           <slot name="footer"></slot>
         </flex-row>
-      </aside> `
+      </aside>
+    `
   }
 }
